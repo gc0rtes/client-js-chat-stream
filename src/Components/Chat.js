befcb5e25ec0c610183ff595256a4b99cb7f3a63
gc0rtes <guilhermeco@gmail.com>
2021-08-13 15:25:02 +0200

    list of channels

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -1,5 +1,27 @@
-const Chat = () => {
-  return <h1>Chat</h1>;
-};
+// const { chatClient, user_id } = require("./client");
 
-export default Chat;
+// const Chat = () => {
+
+
+
+//   const createChannel = async (type, id, members, name) => {
+//     const channel = chatClient.channel(type, id, {
+//       members,
+//       name,
+//     });
+//     return channel.create();
+//   };
+  
+//   createChannel(
+//     "livestream",
+//     "teste-livestream-02",
+//     ["bill", "bob"],
+//     "livestream chat #2"
+//   ).then((r) => console.log(r));
+  
+
+
+//   return <h1>Chat</h1>;
+// };
+
+// export default Chat;
